89403a2a428045e46a91a5559bb2f141478408d8
siphraem <gr.siddhartha@googlemail.com>
2022-11-07 11:36:38 +0100

    added ApiUser and UsersController

---
+++ DNP1_Server/Controllers/ApiClasses/ApiUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace DNP1_Server.Controllers.ApiClasses;
+
+public class ApiUser
+{
+    [Required]
+    [JsonPropertyName("username")]
+    public string UserName { get; set; }
+    
+    [Required]
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
---
+++ DNP1_Server/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+using DNP1_Server.Controllers.ApiClasses;
+using DNP1_Server.Database.Enums;
+using DNP1_Server.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNP1_Server.Controllers; 
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase {
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser([FromBody] ApiUser user) {
+        
+        
+        var dbResponse = Program.Database.CreateUser(user.UserName, user.Password);
+        return dbResponse.Item1 switch {
+            CreateUserEnum.Success => dbResponse.Item2,
+            CreateUserEnum.AlreadyExists => StatusCode(400, "Already exists"),
+            CreateUserEnum.InternalError => StatusCode(500, "Database error"),
+            _ => StatusCode(500, "Unknown server error")
+        };
+    }
+
+    
+}
