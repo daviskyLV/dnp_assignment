2261fe686b3cafb148fcfd484c00bac6f465ff9d
EmiliaNistor <emilia.georgiana24@gmail.com>
2022-11-07 13:49:59 +0100

    ApiLogin

---
+++ DNP1_Server/Controllers/ApiClasses/ApiLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DNP1_Server.Controllers.ApiClasses;
+
+public class ApiLogin {
+    [JsonPropertyName("cookie")]
+    public long Cookie { get; set; }
+    
+    [JsonPropertyName("username")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
