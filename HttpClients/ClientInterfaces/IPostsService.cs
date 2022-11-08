3cedfd441dc3b56de1dc5d44d1725f5bd5d55ca8
EmiliaNistor <emilia.georgiana24@gmail.com>
2022-11-08 14:18:12 +0100

    Post interface

---
+++ HttpClients/ClientInterfaces/IPostsService.cs
@@ -0,0 +1,10 @@
+﻿using DNP1_Server.Controllers.ApiClasses;
+using DNP1_Server.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpCllients.ClientInterfaces;
+
+public interface IPostsService
+{
+    public Task<ActionResult<Post>> CreatePost([FromBody] ApiPost post);
+}
