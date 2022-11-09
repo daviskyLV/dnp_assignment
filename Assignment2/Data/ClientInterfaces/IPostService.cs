394a79405bea8ed7cea9dc9635851812bf9cf8bf
EmiliaNistor <emilia.georgiana24@gmail.com>
2022-11-09 16:39:41 +0100

    Post interface

---
+++ Assignment2/Data/ClientInterfaces/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using DNP1_Server.Controllers.ApiClasses;
+using Microsoft.AspNetCore.Mvc;
+    
+namespace Assignment2.Data.ClientInterfaces;
+
+public interface IPostService
+{
+    public Task<Post> CreatePost(Post submittedPost);
+    public Task<List<Post>> GetAllPosts();
+    public Task<Post> GetPost(long id);
+    
+}
