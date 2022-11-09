f0387ef4f6c89c50b72e9dc307494506c19db2b2
EmiliaNistor <emilia.georgiana24@gmail.com>
2022-11-09 15:12:50 +0100

    Post model

---
+++ Assignment2/Utils/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment2.Utils;
+
+public class Post {
+    public string Id { get; init; }
+    public string Author { get; init; }
+    public string Title { get; init; }
+    public string Body { get; init; }
+}
