2c5ea6e52885f03833d006fb13f1356c0f13e431
Hugo Daniel <82777546+HugoSilva4@users.noreply.github.com>
2022-11-11 22:03:14 +0100

    Final Change
    Fixed User Service

--- Assignment2/Data/Implementations/UsersService.cs
+++ Assignment2/Data/Implementations/UsersService.cs
@@ -22,72 +22,78 @@ public class UsersService : IUsersService
     public async Task<User> Register(User submittedUser)
     {
         // executing request
-        using HttpClient client = new HttpClient(_handler);
-        string userAsJson = JsonSerializer.Serialize(submittedUser);
-        var requestContent = new StringContent(userAsJson, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync(BasePath + "/Users", requestContent);
-
-        // handling error responses
-        if (!response.IsSuccessStatusCode)
+        using (HttpClient client = new HttpClient(_handler))
         {
-            Console.WriteLine("Error while registering user!");
-            Console.WriteLine("JSON request: " + userAsJson);
-            Console.WriteLine("Error code: " + response.StatusCode);
-            Console.WriteLine("Error message: " + await response.Content.ReadAsStringAsync());
-            throw new Exception($@"Failed to register user! Error: {await response.Content.ReadAsStringAsync()}");
+            string userAsJson = JsonSerializer.Serialize(submittedUser);
+            var requestContent = new StringContent(userAsJson, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(BasePath + "/Users", requestContent);
+
+            // handling error responses
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error while registering user!");
+                Console.WriteLine("JSON request: " + userAsJson);
+                Console.WriteLine("Error code: " + response.StatusCode);
+                Console.WriteLine("Error message: " + await response.Content.ReadAsStringAsync());
+                throw new Exception($@"Failed to register user! Error: {await response.Content.ReadAsStringAsync()}");
+            }
+
+            // handling successful responses
+            var responseJsonString = await response.Content.ReadAsStringAsync();
+            var returnedUser = JsonSerializer.Deserialize<User>(responseJsonString);
+
+            if (returnedUser == null)
+                throw new JsonException("Couldn't parse returned user!");
+
+            return returnedUser;
         }
-
-        // handling successful responses
-        var responseJsonString = await response.Content.ReadAsStringAsync();
-        var returnedUser = JsonSerializer.Deserialize<User>(responseJsonString);
-
-        if (returnedUser == null)
-            throw new JsonException("Couldn't parse returned user!");
-
-        return returnedUser;
     }
 
     public async Task<string> Login(User submittedUser)
     {
         // executing request
-        using HttpClient client = new HttpClient(_handler);
-        string userAsJson = JsonSerializer.Serialize(submittedUser);
-        var requestContent = new StringContent(userAsJson, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync(BasePath + "/Login", requestContent);
-
-        // handling error responses
-        if (!response.IsSuccessStatusCode)
+        using (HttpClient client = new HttpClient(_handler))
         {
-            Console.WriteLine("Error while logging in user!");
-            Console.WriteLine("JSON request: " + userAsJson);
-            Console.WriteLine("Error code: " + response.StatusCode);
-            Console.WriteLine("Error message: " + await response.Content.ReadAsStringAsync());
-            throw new Exception($@"Failed to login user! Error: {await response.Content.ReadAsStringAsync()}");
-        }
+            string userAsJson = JsonSerializer.Serialize(submittedUser);
+            var requestContent = new StringContent(userAsJson, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(BasePath + "/Login", requestContent);
 
-        // handling successful responses
-        return await response.Content.ReadAsStringAsync();
+            // handling error responses
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error while logging in user!");
+                Console.WriteLine("JSON request: " + userAsJson);
+                Console.WriteLine("Error code: " + response.StatusCode);
+                Console.WriteLine("Error message: " + await response.Content.ReadAsStringAsync());
+                throw new Exception($@"Failed to login user! Error: {await response.Content.ReadAsStringAsync()}");
+            }
+
+            // handling successful responses
+            return await response.Content.ReadAsStringAsync();
+        }
     }
 
     public async Task<string> Logout(User submittedUser)
     {
         // executing request
-        using HttpClient client = new HttpClient(_handler);
-        string userAsJson = JsonSerializer.Serialize(submittedUser);
-        var requestContent = new StringContent(userAsJson, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync(BasePath + "/Logout", requestContent);
-
-        // handling error responses
-        if (!response.IsSuccessStatusCode)
+        using (HttpClient client = new HttpClient(_handler))
         {
-            Console.WriteLine("Error while logging out user!");
-            Console.WriteLine("JSON request: " + userAsJson);
-            Console.WriteLine("Error code: " + response.StatusCode);
-            Console.WriteLine("Error message: " + await response.Content.ReadAsStringAsync());
-            throw new Exception($@"Failed to logout user! Error: {await response.Content.ReadAsStringAsync()}");
-        }
+            string userAsJson = JsonSerializer.Serialize(submittedUser);
+            var requestContent = new StringContent(userAsJson, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(BasePath + "/Logout", requestContent);
 
-        // handling successful responses
-        return await response.Content.ReadAsStringAsync();
+            // handling error responses
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error while logging out user!");
+                Console.WriteLine("JSON request: " + userAsJson);
+                Console.WriteLine("Error code: " + response.StatusCode);
+                Console.WriteLine("Error message: " + await response.Content.ReadAsStringAsync());
+                throw new Exception($@"Failed to logout user! Error: {await response.Content.ReadAsStringAsync()}");
+            }
+
+            // handling successful responses
+            return await response.Content.ReadAsStringAsync();
+        }
     }
 }
